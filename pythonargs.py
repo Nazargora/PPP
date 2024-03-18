d28dd4c67ac3bb072ba52dd69d7082b43eb5e122
Nazar <111236875+Nazargora@users.noreply.github.com>
2024-03-18 19:24:36 +0200

    Create pythonargs.py

---
+++ pythonargs.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import shutil
+
+def install_flows(changed_files):
+    print("Installing Flows")
+    
+    if changed_files:
+        spec_files = [file for file in os.listdir("specs") if file.endswith(".json")]
+        
+        files_to_copy = [file for file in changed_files if file in spec_files]
+        
+        if not files_to_copy:
+            print("No matching files found in 'specs' directory. Skipping installation.")
+        else:
+            create_flow_spec(files_to_copy)
+    else:
+        print("No changed files provided. Skipping installation.")
+
+def create_flow_spec(files_to_copy):
+    if not os.path.exists("specs2"):
+        os.makedirs("specs2")
+    
+    for file_name in files_to_copy:
+        src = os.path.join("specs", file_name)
+        dest = os.path.join("specs2", file_name)
+        
+        # Check if the file already exists in specs2
+        if not os.path.exists(dest):
+            shutil.copy(src, dest)
+            print(f"Copied {file_name} to 'specs2' directory")
+        else:
+            print(f"File {file_name} already exists in 'specs2' directory")
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description='Add arg change')
+    arg_parser.add_argument('-cf', '--changed_files', nargs='+', required=True, help='List of space-separated changed file names')
+    args = arg_parser.parse_args()
+    changed_files = args.changed_files
+    install_flows(changed_files)
